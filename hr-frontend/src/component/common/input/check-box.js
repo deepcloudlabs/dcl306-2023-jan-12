c2243ae8767fee26dc55afbfaa6361a6e32eb486
Binnur KURT <deepcloudlabs@gmail.com>
2023-01-14 11:03:28 +0300

    Create check-box.js

---
+++ hr-frontend/src/component/common/input/check-box.js
@@ -0,0 +1,15 @@
+export default function CheckBox({id, label, handleChange, value}) {
+    return (
+        <div className="form-check">
+            <label className="form-check-label">
+                {label}
+                <input type="checkbox"
+                       id={id}
+                       name={id}
+                       onChange={handleChange}
+                       value={value} className="form-check-input">
+                </input>
+            </label>
+        </div>
+    );
+}
