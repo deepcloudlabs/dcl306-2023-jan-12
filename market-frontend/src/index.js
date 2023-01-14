f295d26e6b013177cc9951c92c96ec9c27bbe7d4
Binnur KURT <deepcloudlabs@gmail.com>
2023-01-14 14:55:11 +0300

    refactor

--- market-frontend/src/index.js
+++ market-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css";
-import Market from "./App";
+import Market from "./market";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
