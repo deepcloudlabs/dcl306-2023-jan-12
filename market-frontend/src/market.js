9e7883a4bf93c53519badc2df8ac39328fa18fd2
Binnur KURT <deepcloudlabs@gmail.com>
2023-01-14 15:48:03 +0300

    Update market.js

--- market-frontend/src/market.js
+++ market-frontend/src/market.js
@@ -4,23 +4,33 @@ import CardHeader from "./component/common/card/card-header";
 import CardBody from "./component/common/card/card-body";
 import FormGroup from "./component/common/output/form-group";
 import SelectBox from "./component/common/input/select-box";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Label from "./component/common/output/label";
 import Button from "./component/common/input/button";
 import {tradeData} from "./utility/market-util";
 import TableHeader from "./component/common/table/table-header";
 import TableBody from "./component/common/table/table-body";
 import Table from "./component/common/table/table";
+import io from "socket.io-client";
 
+const socket = io("ws://localhost:5555");
 function Market() {
+    const [symbol, setSymbol] = useState("BTCUSDT");
     const [windowSize, setWindowSize] = useState(25);
-    const [trades, setTrades] = useState(tradeData);
+    const [trades, setTrades] = useState([]);
     const [monitoringButtonState, setMonitoringButtonState] = useState({
         status: false,
         bgColor: "bg-success",
         label: "Start"
     });
+    useEffect(()=>{
+        socket.on("trade", (trade)=>{
+            setTrades([...trades, trade]);
+        });
+        return ()=>{
 
+        }
+    });
     function changeMonitoring(event) {
         const newMonitoringButtonState = {...monitoringButtonState};
 
@@ -41,11 +51,18 @@ function Market() {
                 <CardHeader title="Market Data"/>
                 <CardBody>
                     <FormGroup className="form-floating">
+                        <SelectBox id="symbol"
+                                   value={symbol}
+                                   options={["BTCUSDT", "ETHBTC", "BNBBTC"]}
+                                   handleChange={event => setSymbol(event.target.value)}/>
+                        <Label label="Symbol" htmlFor="symbol"></Label>
+                    </FormGroup>
+                    <FormGroup className="form-floating">
                         <SelectBox id="windowSize"
                                    value={windowSize}
                                    options={[25, 50, 100, 250]}
                                    handleChange={event => setWindowSize(event.target.value)}/>
-                        <Label label="Wind Size" htmlFor="windSize"></Label>
+                        <Label label="Window Size" htmlFor="windowSize"></Label>
                     </FormGroup>
                     <FormGroup>
                         <Button id="stopMonitor"
@@ -58,9 +75,9 @@ function Market() {
                             <TableHeader columns="Sequence,Trade ID,Symbol,Price,Quantity,Volume,Timestamp"/>
                             <TableBody>
                                 {
-                                    trades.map((trade,idx) =>
+                                    trades.map((trade, idx) =>
                                         <tr key={trade.sequence}>
-                                            <td>{trade.sequence}</td>
+                                            <td>{trade.sequence * idx}</td>
                                             <td>{trade.tradeId}</td>
                                             <td>{trade.symbol}</td>
                                             <td>{trade.price}</td>
