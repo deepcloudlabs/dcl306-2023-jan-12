864f38f8f4d2e088073b2269e07ba1ff4814afce
Binnur KURT <deepcloudlabs@gmail.com>
2023-01-14 14:58:01 +0300

    Update market.js

--- market-frontend/src/market.js
+++ market-frontend/src/market.js
@@ -1,8 +1,19 @@
-function Market() {
-    return (
-        <div>
+import Container from "./component/common/card/container";
+import Card from "./component/common/card/card";
+import CardHeader from "./component/common/card/card-header";
+import CardBody from "./component/common/card/card-body";
 
-        </div>
+function Market() {
+
+    return (
+        <Container>
+            <Card>
+                <CardHeader title="Market Data"/>
+                <CardBody>
+
+                </CardBody>
+            </Card>
+        </Container>
     );
 }
 
